27bb04b223ddd51fa92dc9526a722a3cf5a15e2c
stefanylyra <142688044+stefanylyra@users.noreply.github.com>
2026-07-16 11:08:14 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,46 @@
 
+const config = {
+
+    type: Phaser.AUTO,
+
+    width: 1280,
+
+    height: 720,
+
+    backgroundColor: "#87CEEB",
+
+    scene:{
+
+        preload,
+
+        create,
+
+        update
+
+    }
+
+}
+
+const game = new Phaser.Game(config);
+
+function preload(){
+
+}
+
+function create(){
+
+    this.add.text(
+        30,
+        30,
+        "Cidade em Evolução",
+        {
+            fontSize:"36px",
+            color:"#000"
+        }
+    );
+
+}
+
+function update(){
+
+}
